4acf4fb40f7d6155c6d1c2f0c150f9cb5fd06cb3
dacortess <dcortessalazar@gmail.com>
2022-09-21 01:13:55 -0500

    fix: start

--- start.sh
+++ start.sh
@@ -3,15 +3,11 @@
     cd logic
     python ../main.py
     cd ..
-    #save your output
-
 } || { # catch
     { # try
         cd logic
         python3 ../main.py
         cd ..
-        #save your output
-
     } || { # catch
         :
     }
